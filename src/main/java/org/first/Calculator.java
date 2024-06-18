ceab3d53329861ba6e12b7cfbd6257d00da3f360
LisaDev-Java <pshenichnaya-01@list.ru>
2024-06-18 23:07:15 +0400

    tabulation fixit

--- src/main/java/org/first/Calculator.java
+++ src/main/java/org/first/Calculator.java
@@ -6,25 +6,25 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Input two number: ");
-            double num1 = scanner.nextDouble();
-            double num2 = scanner.nextDouble();
-            System.out.print("Input a action (/ * - +) : ");
-            String sign = scanner.next();
+        System.out.print("Input two number: ");
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+        System.out.print("Input a action (/ * - +) : ");
+        String sign = scanner.next();
 
-            if (sign.equals("/") ) {
-                System.out.println(num1 / num2);
-            }
-            if (sign.equals("*") ) {
-                System.out.println(num1 * num2);
-            }
-            if (sign.equals("+") ) {
-                System.out.println(num1 + num2);
-            }
-            if (sign.equals("-")) {
-                System.out.println(num1 - num2);
-            }
+        if (sign.equals("/")) {
+            System.out.println(num1 / num2);
+        }
+        if (sign.equals("*")) {
+            System.out.println(num1 * num2);
+        }
+        if (sign.equals("+")) {
+            System.out.println(num1 + num2);
+        }
+        if (sign.equals("-")) {
+            System.out.println(num1 - num2);
         }
     }
+}
